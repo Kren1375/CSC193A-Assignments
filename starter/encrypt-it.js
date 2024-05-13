acf37c66e66cdf6d00a7e439b072a374c9623281
Kren1375 <121604663+Kren1375@users.noreply.github.com>
2024-05-13 13:21:17 -0700

    Added Assignment 6 on consoles and ciphers

---
+++ starter/encrypt-it.js
@@ -0,0 +1,73 @@
+/*
+ * Starter file 
+ */
+(function () {
+  "use strict";
+
+  /**
+   * The starting point in our program, setting up a listener
+   * for the "load" event on the window, signalling the HTML DOM has been constructed
+   * on the page. When this event occurs, the attached function (init) will be called.
+   */
+  window.addEventListener("load", init);
+
+  const clicker = document.querySelector("encrypt-it");
+  const reset = document.querySelector("#reset");
+  /**
+   * TODO: Write a function comment using JSDoc.
+   */
+  function init() {
+    // Note: In this function, we usually want to set up our event handlers
+    // for UI elements on the page.
+    console.log("Window loaded!");
+
+    const clicker = document.querySelector("#encrypt-it");
+    const reset = document.querySelector("#reset");
+
+    clicker.addEventListener('click', () => { handleClick(); shiftText(); });
+    reset.addEventListener('click', () => { handleReset(); });
+
+
+  }
+
+  // Add any other functions in this area (you should not implement your
+  // entire program in the init function, for similar reasons that
+  // you shouldn't write an entire Java program in the main method).
+  function handleClick() {
+    console.log("Button clicked!");
+
+  }
+
+  function handleReset() {
+    document.getElementById("input-text").value = '';
+
+  }
+
+  function shiftText() {
+    let text = document.getElementById("input-text").value;
+    document.getElementById("result").innerHTML = shiftCipher(text);
+  }
+  /**
+ * Returns an encrypted version of the given text, where
+ * each letter is shifted alphabetically ahead by 1 letter,
+ * and 'z' is shifted to 'a' (creating an alphabetical cycle).
+ */
+  function shiftCipher(text) {
+    text = text.toLowerCase();
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] < 'a' || text[i] > 'z') {
+        result += text[i];
+      } else if (text[i] == 'z') {
+        result += 'a';
+      } else { // letter is between 'a' and 'y'
+        let letter = text.charCodeAt(i);
+        let resultLetter = String.fromCharCode(letter + 1);
+        result += resultLetter;
+      }
+    }
+    return result;
+  }
+
+
+})();
